e0a0f5bb286cfce855cf94984911ccf249b8b6be
Nick Kamer <nick@ziaomedia.com>
2023-05-16 01:09:49 +0200

    feat: set up react router

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./lib/router.tsx";
+
+interface AppProps {}
+export const App: FC<AppProps> = ({}) => {
+    return (
+        <div>
+            <RouterProvider router={router} />
+        </div>
+    );
+};
