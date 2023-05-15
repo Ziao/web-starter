6f833ebf3b42c33b25c40be4a898554f93d8fcf9
Nick Kamer <nick@ziaomedia.com>
2023-05-16 01:04:32 +0200

    feat: add heroicons and clean up assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
+import { ClockIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
     const [count, setCount] = useState(0);
@@ -8,12 +7,7 @@ function App() {
     return (
         <>
             <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
+                <ClockIcon className="h-6 w-6 text-blue-500" />
             </div>
             <h1>Vite + React</h1>
             <div className="card">
