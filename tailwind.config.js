5672e23e89c4b89c81257db13e2efa26add70f24
cosmic-fox <arya@cosmicfox.io>
2023-05-25 22:08:49 +0200

    chore: tailwind config important true

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     // eslint-disable-next-line no-undef
     plugins: [require("daisyui")],
+    important: true,
     theme: {
         container: {
             center: true,
