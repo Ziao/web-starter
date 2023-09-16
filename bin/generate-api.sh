44a227c60bd7db6068375be0bfb48c5bed2d9ecc
Nick <nick@ziaomedia.com>
2023-09-16 03:00:55 +0200

    chore: clean up openapi trash

--- bin/generate-api.sh
+++ bin/generate-api.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# Generate API
 openapi-generator generate \
 --generator-name typescript-axios \
 --output ./src/api \
 -i http://localhost:5000/api-json \
 --additional-properties=modelPackage=models,apiPackage=api,withSeparateModelsAndApi=true,supportsES6=true,withInterfaces=true
 
+# Clean up stuff we don't need
+rm -rf ./src/api/.openapi-generator
+rm -rf ./src/api/git_push.sh
+rm -rf ./src/api/.gitignore
+rm -rf ./src/api/.npmignore
+rm -rf ./src/api/.openapi-generator-ignore
+
+# Format files
 yarn format
 
 # General options
